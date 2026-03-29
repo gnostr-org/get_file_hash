000139c690f10576e782d705105d1609e5029767
randymcmillan <randymcmillan@protonmail.com>
2026-03-28 21:13:59 -0400

    feat(build): add a minimal build.rs script
    00-00002882

---
+++ build.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // This build script does nothing specific for now, but it will be executed before compilation.
+    // You can add logic here to, for example, generate code, link libraries, or set environment variables.
+    println!("cargo:warning=Running build.rs script...");
+}
