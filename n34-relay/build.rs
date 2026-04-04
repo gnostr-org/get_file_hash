91846421a4c5f4e01618a5211b5f9352b408b359
randymcmillan <randymcmillan@protonmail.com>
2026-04-04 18:49:48 -0400

    v0.4.3+n34

--- n34-relay/build.rs
+++ n34-relay/build.rs
@@ -1,9 +1,18 @@
-fn main() {
+#[cfg(feature = "gen-protos")]
+fn compile_protos() {
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
         .build_transport(true)
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&["proto/plugins.proto"], &["proto"])
-        .expect("protoc is required to build n34-relay. Install protobuf or set PROTOC to the protoc binary path.");
+        .expect("protoc is required");
+}
+
+#[cfg(not(feature = "gen-protos"))]
+fn compile_protos() {}
+
+
+fn main() {
+    compile_protos();
 }
